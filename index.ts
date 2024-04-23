a7ddfde8c1fac6aefc2e461847b287788356fc7f
jnwync <jonwayne.cabusbusan@gmail.com>
2024-04-23 14:38:01 +0800

    added cors for frontend

--- index.ts
+++ index.ts
@@ -1,15 +1,22 @@
-import express from 'express';
-import { router } from './routes/routes';
+import express from "express";
+import cors from "cors";
+import { router } from "./routes/routes";
 
 export const app = express();
+
+const corsOptions = {
+  origin: "http://localhost:5173",
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/', router)
+app.use("/", router);
 
 function startServer() {
-    app.listen(3000, () => {
-        console.log('Server is running on http://localhost:3000')
-    });
+  app.listen(3000, () => {
+    console.log("Server is running on http://localhost:3000");
+  });
 }
 
-startServer();  
+startServer();
